c6b1cebf4bcc3c4b738475342e89918588fb2ce9
Redouane <48132401+Rodoudou@users.noreply.github.com>
2020-02-03 15:05:38 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="nav">
+        <ul>
+          <a src="https://wwww.yourchef.paris">
+            <li>lien 1</li>
+          </a>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Header;
